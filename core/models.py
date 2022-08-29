6aff10bec02f0925b54429f86417a1121da419bf
Venturini <ana146claraventurini@gmail.com>
2022-08-29 11:46:50 -0300

    Chaves Estrangeiras

--- core/models.py
+++ core/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 
 class Marca(models.Model):
-    nome = models.CharField(max_length=50)
+    nome = models.CharField(max_length=255)
 
     def __str__(self):
         return self.nome
+
+    class Meta:
+        verbose_name_plural = "Marcas"
 
 
 class Categoria(models.Model):
@@ -13,3 +16,12 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao
+
+class Carro(models.Model):
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32, null=True, blank=True)
+    quantidade = models.IntegerField()
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.titulo} ({self.quantidade})'
